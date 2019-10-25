26d8e470e6a263c57306037727c79845fa4c241e
Nazar Skoropad <nazar.skoropad@icloud.com>
2019-10-25 21:56:50 +0300

    Initial commit

---
+++ Emap-offlinePart/Emap-offlinePart/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emap_offlinePart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
