2bf2ac358dd47374852286939a8b95d9864e79e7
Nazar Skoropad <nazar.skoropad@icloud.com>
2019-10-26 18:34:20 +0300

    Added console reader

---
+++ Emap-offlinePart/Reader/ConsoleReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Reader
+{
+    public class ConsoleReader : IReader
+    {
+        string IReader.ReadLine()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
