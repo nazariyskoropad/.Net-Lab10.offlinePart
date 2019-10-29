c2981e5b190b3fefd08ba9ed86dca18e9cec4a25
Nazar Skoropad <nazar.skoropad@icloud.com>
2019-10-29 12:36:27 +0200

    Added FilePrinter

---
+++ Emap-offlinePart/Printer/FilePrinter.cs
@@ -0,0 +1,33 @@
+﻿using Printer.Properties;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Printer
+{
+    public class FilePrinter : IPrinter
+    {
+        private string filePath = Settings.Default.filePath;
+        void IPrinter.Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IPrinter.Print(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IPrinter.PrintLine(string text)
+        {
+            using (StreamWriter sw = new StreamWriter(filePath, true))
+            {               
+                sw.WriteLine(text);
+                sw.Close();
+            }
+        }
+    }
+}
